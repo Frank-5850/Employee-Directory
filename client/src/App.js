deeb427ccadf3bc21c08b8d06eb979035184a51a
Franco <phanfranco@yahoo.com>
2021-03-19 03:44:32 -0700

    Installed axios and concurrently npm packages. Created connection between the front and back ends.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,20 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  useEffect(() => {
+    (async () => {
+      try {
+        axios.get("/test");
+        console.log("success");
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+
+    axios.get("/test");
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get("/test", async (req, res) => {
   try {
+    console.log("success");
     res.send("success");
   } catch (err) {
     console.log("error:", err);
