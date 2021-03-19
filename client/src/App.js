8fd6f4670336a5be3424a5f9397337e23283d758
Franco <phanfranco@yahoo.com>
2021-03-19 05:36:57 -0700

    Successfully rendered generated users to front end.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
     (async () => {
       try {
         const randomUser = await axios.get(
-          "https://randomuser.me/api/?results=50"
+          "https://randomuser.me/api/?results=20"
         );
         console.log(randomUser.data.results);
+        setUsers(randomUser.data.results);
       } catch (err) {
         console.log(err);
       }
     })();
-
     axios.get("/test");
   }, []);
 
+  console.log(users);
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      {users.map((user) => (
+        <div>
+          <p>
+            Name: {user.name.first} {user.name.last} Email: {user.email} {""}
+            Location: {user.location.country} Username: {user.login.username}{" "}
+            Age: {user.dob.age} Gender: {user.gender}
+          </p>
+          <p>{user.name.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
