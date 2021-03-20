1819c10dd693b0a2f420693cfabd303822b9679b
Franco <phanfranco@yahoo.com>
2021-03-20 01:20:12 -0700

    Was able to filter and find a specific user.

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,28 @@ import axios from "axios";
 function App() {
   const [users, setUsers] = useState([]);
 
+  const onChange = (e) => {
+    e.preventDefault();
+    let filterUsers = users.filter((user) => {
+      let firstName = user.name.first;
+      let lastName = user.name.last;
+      return (
+        firstName.includes(e.target.value) || lastName.includes(e.target.value)
+      );
+    });
+    console.log(filterUsers);
+    setUsers(filterUsers);
+  };
+
+  const submitButton = (e) => {
+    e.preventDefault();
+  };
+
   useEffect(() => {
     (async () => {
       try {
         const randomUser = await axios.get(
-          "https://randomuser.me/api/?results=20"
+          "https://randomuser.me/api/?nat=us,dk,fr,gb,ca,nz&results=3 "
         );
         console.log(randomUser.data.results);
         setUsers(randomUser.data.results);
@@ -23,8 +40,14 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      {users.map((user) => (
-        <div>
+      <form>
+        <input onChange={onChange} type="text" name="userSearch" />{" "}
+        <button onClick={submitButton}>Search</button>
+      </form>
+
+      {users.map((user, index) => (
+        <div key={index}>
+          <img src={user.picture.large} />
           <p>
             Name: {user.name.first} {user.name.last} Email: {user.email} {""}
             Location: {user.location.country} Username: {user.login.username}{" "}
