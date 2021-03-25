dc0318fb79332ddb141a464d91b13e004a3bb6cb
Franco <phanfranco@yahoo.com>
2021-03-25 00:20:00 -0700

    Added Header.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Header from "./components/Header";
 
 function App() {
   const [users, setUsers] = useState([]);
 
   const onChange = (e) => {
     e.preventDefault();
-    let filterUsers = users.filter((user) => {
+    let filterUsers = filteredArray.filter((user) => {
       let firstName = user.name.first;
       let lastName = user.name.last;
       return (
         firstName.includes(e.target.value) || lastName.includes(e.target.value)
       );
     });
-    console.log(filterUsers);
+    console.log(filteredArray);
     setUsers(filterUsers);
   };
+
+  let filteredArray;
 
   const submitButton = (e) => {
     e.preventDefault();
@@ -27,7 +30,8 @@ function App() {
         const randomUser = await axios.get(
           "https://randomuser.me/api/?nat=us,dk,fr,gb,ca,nz&results=3 "
         );
-        console.log(randomUser.data.results);
+        filteredArray = randomUser.data.results;
+        console.log(filteredArray);
         setUsers(randomUser.data.results);
       } catch (err) {
         console.log(err);
@@ -39,7 +43,7 @@ function App() {
   console.log(users);
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Header />
       <form>
         <input onChange={onChange} type="text" name="userSearch" />{" "}
         <button onClick={submitButton}>Search</button>
@@ -53,7 +57,6 @@ function App() {
             Location: {user.location.country} Username: {user.login.username}{" "}
             Age: {user.dob.age} Gender: {user.gender}
           </p>
-          <p>{user.name.email}</p>
         </div>
       ))}
     </div>
---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1> Employee directory!</h1>
+    </div>
+  );
+};
+
+export default Header;
